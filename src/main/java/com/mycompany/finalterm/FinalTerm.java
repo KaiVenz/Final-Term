b694b5f2867017f9feb1e6029f76602790b50967
DESKTOP-FSL1LNB\User <gavinoheaven06@gmail.com>
2024-10-14 10:35:55 +0800

    MULTIDIMENSIONAL ACTIVITY 2 UPDATE! FOUS STARS!

--- src/main/java/com/mycompany/finalterm/FinalTerm.java
+++ src/main/java/com/mycompany/finalterm/FinalTerm.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.finalterm;
 
-/**
- *
- * @author User
- */
+
 public class FinalTerm {
 
     public static void main(String[] args) {
